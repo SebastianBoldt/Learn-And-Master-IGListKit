d49d2e1c409335fc9623d9757e4e1cb2e8f3ba44
Sebastian Boldt <self.dealloc@googlemail.com>
2018-03-06 07:54:29 +0100

    Initial Commit

---
+++ ModernLists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModernLists
+//
+//  Created by Sebastian Boldt on 06.03.18.
+//  Copyright © 2018 Sebastian Boldt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
