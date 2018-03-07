43098433ca47ef0ef020fbf9af334fc1981c6b41
Sebastian Boldt <self.dealloc@googlemail.com>
2018-03-07 09:08:31 +0100

    typo fix

--- LearnAndMasterIGListKit/Constants.swift
+++ LearnAndMasterIGListKit/Constants.swift
@@ -7,7 +7,7 @@ import IGListKit
 
 struct Constants {
     static let objects: [ListDiffable] = [SuperHero(firstName: "Peter", lastName: "Parker", superHeroName: "SpiderMan", icon: "🕷"),
-                             Ad(description: "Want to know what your future? 🔮"),
+                             Ad(description: "Do you want to know what your future looks like? 🔮"),
                              SuperHero(firstName: "Bruce", lastName: "Wayne", superHeroName: "Batman", icon: "🦇"),
                              SuperHero(firstName: "Tony", lastName: "Stark", superHeroName: "Ironman", icon: "🤖"),
                              SuperHero(firstName: "Bruce", lastName: "Banner", superHeroName: "Incredible Hulk", icon: "🤢")]
